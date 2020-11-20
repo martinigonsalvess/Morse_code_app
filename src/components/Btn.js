669b308d7b3b72030fc3510cbd87a01cc7d38eb8
Martin <martinigonsalves@gmail.com>
2020-11-20 01:51:45 +0100

    layout and buttons

---
+++ src/components/Btn.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './StyleBtn.css';
+
+function Btn() {
+    return (
+        <>
+            <div class="button-container">
+                <button class="btn1">1</button>
+                <button class="btn1">2</button>
+                <button class="btn1">3</button>
+                <button class="btn1">4</button>
+                <button class="btn1">5</button>
+                <button class="btn1">6</button>
+                <button class="btn1">7</button>
+                <button class="btn1">8</button>
+                <button class="btn1">9</button>
+                <button class="btn1">0</button>
+
+                <button class="btn1">A</button>
+                <button class="btn1">B</button>
+                <button class="btn1">C</button>
+                <button class="btn1">D</button>
+                <button class="btn1">E</button>
+                <button class="btn1">F</button>
+                <button class="btn1">G</button>
+                <button class="btn1">H</button>
+                <button class="btn1">I</button>
+                <button class="btn1">J</button>
+
+                <button class="btn1">K</button>
+                <button class="btn1">L</button>
+                <button class="btn1">M</button>
+                <button class="btn1">N</button>
+                <button class="btn1">O</button>
+                <button class="btn1">P</button>
+                <button class="btn1">Q</button>
+                <button class="btn1">R</button>
+                <button class="btn1">S</button>
+                <button class="btn1">T</button>
+
+                <button class="btn1">U</button>
+                <button class="btn1">V</button>
+                <button class="btn1">W</button>
+                <button class="btn1">X</button>
+                <button class="btn1">Y</button>
+                <button class="btn1">Z</button> 
+                <button class="btn1">Space</button>         
+        
+
+
+
+
+
+            </div>
+        </>
+    );
+
+
+}
+export default Btn;
---
+++ src/components/Content.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './StyleContent.css';
+
+function Content(){
+    return(
+        <>
+        
+        <div className="content">
+            <div id="upper_left">n</div>
+            <div id="upper_right">n</div>
+            <div id="lower_left">n</div>
+            <div id="lower_right">n</div>
+
+            <div className="content-container">
+                <div className="content-input-top-icon">w</div>
+                <div className="content-input-text"></div>
+                <div className="content-output-morse"></div>
+                <div className="content-alphabeth-buttons"></div>
+                <div className="footer-about-us"></div>
+
+
+            </div>  
+         
+        </div>    
+        </>
+
+    )
+}
+export default Content;
